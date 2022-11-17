08e5cdecefa89eaf79fb4fdf6801d3dad941b21a
Dayvson do Carmo Gomes <90016718+DayvsonGomes@users.noreply.github.com>
2022-11-17 00:36:29 +0000

    passo 2 e 3 virando funçao conectar

--- Aula4-2022-11-16c.py
+++ Aula4-2022-11-16c.py
@@ -1,23 +1,13 @@
 # passo 1 - importar a biblioteca sqlite3
 import sqlite3
 
-# passo 2: vamos estabelecer uma conexão com o banco
-conexao=sqlite3.connect("dc_universe.db")
+# passo 2 e 3: virarão funçao conectar
+def conectar():
+  # passo 2: cestabelecer conexao com o BD
+  conexao=sqlite3.connect("dc_universe.db")
 
-# passo 3: criar um objeto do tipo cursor
-cursor = conexao.cursor()
+  # passo 3: criar um objeto to tipo cursor
+  cursor = conexao.cursor()
 
-# passo4: Comando SQL do banco
-sql = "SELECT pessoa_id, nome,nome_civil, tipo FROM pessoas"
-
-# passo 5: colocar o camando SQL dentro da variavel String
-cursor.execute(sql)
-
-# passo 6: exibir a consulta com todos os nomes de heróis e vilões
-pessoas = cursor.fetchall()
-for pessoa in pessoas:
-  print(pessoa)
-
-for pessoa in pessoas:
-  print(f'Nome:{pessoa[1]}({pessoa[2]})')
+  return conexao, cursor
 
